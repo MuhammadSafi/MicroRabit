e22b5a8447485a98ed91d594b2cf3065a99f36c6
Muhammad Safi <m.safics@hotmail.com>
2020-04-12 01:10:34 +0400

    launch micro service with swagger

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService) {
+
+            _accountService = accountService;
+        }
+
+        //private readonly ILogger<BankingController> _logger;
+
+        //public BankingController(ILogger<BankingController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+            //Get Api /Banking
+        [HttpGet]
+        public ActionResult<Account> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
